8169a1d5ecb567eec43144d3ce192a12d9b3f842
Chenhe Yuan <chenheyuan@Chenhes-MacBook-Pro.local>
2024-11-20 09:14:37 -0600

    added javadoc comments for data parser

--- src/DataParser.java
+++ src/DataParser.java
@@ -1,7 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * parses student data from a file and then returns a list of university students
+ */
 public class DataParser {
+    
+    /**
+     * Parses a file containing student data and creates a list of UniversityStudent objects.
+     *
+     * @param filename the name of the file containing student data
+     * @return a list of UniversityStudent objects
+     * @throws IOException if an error occurs while reading the file
+     */
     public static List<UniversityStudent> parseStudents(String filename) throws IOException {
         return new ArrayList<>();
     }
