4d1f9939ee4b21e5d4c86111df00650376ce82f4
Chenhe Yuan <chenheyuan@Chenhes-MacBook-Pro.local>
2024-11-20 09:17:03 -0600

    added galeshapley class javadoc comments

--- src/GaleShapley.java
+++ src/GaleShapley.java
@@ -1,6 +1,15 @@
 import java.util.*;
 
+/**
+ * the Gale-Shapley algorithm main logic for assigning roommates to students based on their preferences.
+ */
 public class GaleShapley {
+    /**
+     * Assigns roommates to students using thsi algorithm.
+     *
+     * @param students the list of students 
+     */
     public static void assignRoommates(List<UniversityStudent> students) {
+        // Method implementation
     }
 }
