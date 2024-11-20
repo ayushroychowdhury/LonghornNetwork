7c1f5576b4e2ef7f37a1a171c694851617ed0f4b
Chenhe Yuan <chenheyuan@Chenhes-MacBook-Pro.local>
2024-11-20 09:16:01 -0600

    added javadoc comments for friend request thread class

--- src/FriendRequestThread.java
+++ src/FriendRequestThread.java
@@ -1,8 +1,21 @@
+/**
+ * a thread to handle sending a friend request between two university students.
+ */
 public class FriendRequestThread implements Runnable {
+    /**
+     * make a FriendRequestThread with a sender and a receiver.
+     *
+     * @param sender   the student sending the friend request
+     * @param receiver the student receiving the friend request
+     */
     public FriendRequestThread(UniversityStudent sender, UniversityStudent receiver) {
         // Constructor
     }
 
+    /**
+     * Runs the thread to handle the friend request.
+     * Executes the logic for sending the request.
+     */
     @Override
     public void run() {
         // Method signature only
